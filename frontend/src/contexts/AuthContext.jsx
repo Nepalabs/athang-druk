ed4fbb9ce3c44db9aca16505b30a16f36b86e7f0
Tashi Yuden <tashiyuden07@gmail.com>
2025-06-13 10:37:05 +0600

    Have added the AuthContext and AppRoutes.jsx

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -1,1 +1,77 @@
-//
+import axios from "axios";
+import { createContext, useContext, useEffect, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [user, setUser] = useState({});
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getLoggedInUser();
+  }, []);
+
+  const getLoggedInUser = async () => {
+    try {
+      setLoading(true);
+      const token = localStorage.getItem("lwp-token");
+      if (!token) {
+        setLoggedIn(false);
+        setLoading(false);
+        return;
+      }
+
+      const response = await axios.get(
+        "http://localhost:3000/auth/loggedin-user",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      setLoggedIn(true);
+      setUser(response.data.user);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      setLoggedIn(false);
+      setLoading(false);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      const token = localStorage.getItem("lwp-token");
+      if (!token) {
+        setLoggedIn(false);
+        setLoading(false);
+        return;
+      }
+
+      await axios.delete("http://localhost:3000/auth/singout", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      setLoggedIn(false);
+      setUser({});
+      localStorage.setItem("lwp-token", "");
+    } catch (error) {
+      console.error(error);
+      setLoggedIn(false);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ isLoggedIn, user, isLoading, getLoggedInUser, logout }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+export const useAuth = () => useContext(AuthContext);
