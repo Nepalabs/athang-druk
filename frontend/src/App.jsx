74d59fb6b2128cd030f96e90c208ed292961edf7
slhendup <sonamlhendupjr@gmail.com>
2025-06-24 09:43:59 +0600

    used the axios config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
 import AppRoutes from "./routes/AppRoutes";
 import "./App.css";
+import axios from "./config/axiosConfig";
 
 function App() {
   const [healthStatus, setHealthStatus] = useState("Checking...");
 
   useEffect(() => {
     axios
-      .get("http://localhost:3000/health")
+      .get("/health")
       .then((response) => setHealthStatus(response.data))
       .catch(() => setHealthStatus("Failed to fetch health status"));
   }, []);
